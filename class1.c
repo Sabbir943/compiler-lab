9d4368b4bd0703cdc4747db5b88e150345663ad0
Md Sabbir Ahmad <mdsabbirahmad943@gmail.com>
2025-11-03 10:46:41 +0600

    Add files via upload
    second class upadted

--- class1.c
+++ class1.c
@@ -18,6 +18,11 @@ int isKeyword(char *str)
     return 0;
 }
 
+int isOperator(char ch)
+{
+    return (ch=='-'||ch=='+'||ch=='*'||ch=='/'||ch=='%'||ch=='='||ch=='<'||ch=='>'||ch=='!');
+}
+
 int main()
 {
     char str[100];
@@ -56,5 +61,55 @@ int main()
             }
 
         }
+        //number
+        else if(isdigit(str[i])){
+            char temp[50];
+            int k=0;
+
+            while(isdigit(str[i])){
+                temp[k++]=str[i++];
+            }
+            temp[k]='\0';
+
+            printf("Number --> : %s\n", temp);
+        }
+        //operator
+
+        else if(isOperator(str[i])){
+            char op =str[i];
+
+            //multi operator
+            if((op=='='||op=='<'||op=='>'||op=='!')
+               && str[i+1]=='=')
+               {
+                   printf("Operator -->%c%c\n", str[i],str[i++]);
+                   i+=2;
+               }
+               else{
+                 printf("Operator -->%c\n", str[i]);
+                 i++;
+               }
+
+        }
+        //parenthesis/punchuator
+
+        else if(str[i]=='('||str[i]==')'||str[i]=='{'||str[i]=='}'||str[i]==';'){
+                printf("Punchuator --> %c\n", str[i]);
+                i++;
+
+        }
+
+        else{
+            printf("Symbol -->%c\n", str[i]);
+            i++;
+        }
     }
+
+
+
+
+
+
+
 }
+
